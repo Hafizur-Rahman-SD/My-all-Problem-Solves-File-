6e0dc5ab25346abcf13860a332814cc61020201f
Hafizur Rahman <hafizurrahman5004@gmail.com>
2025-06-02 17:59:36 +0600

    Update and rename Python Problem Solves to Python Problem Solves.py

---
+++ Python Problem Solves.py
@@ -0,0 +1,9 @@
+
+'''Problem 1: Longest Substring Without Repeating Characters
+Problem:
+You are given a string. Find the length of the longest part of the string that has all different characters.
+
+Example:
+Input: "abcabcbb"
+Output: 3
+Explanation: The longest part with all different letters is "abc".'''
